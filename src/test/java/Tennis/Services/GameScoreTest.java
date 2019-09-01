2afc244896af51724949fe34ba312ede78f95016
james ioppolo <james.ioppolo@gmail.com>
2019-09-01 14:49:15 +1000

    Removed redundant test declaration

--- src/test/java/Tennis/Services/GameScoreTest.java
+++ src/test/java/Tennis/Services/GameScoreTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import junitparams.JUnitParamsRunner;
 
-@RunWith(JUnitParamsRunner.class)
 public class GameScoreTest {
 
     private GameScore gameScore = new GameScore();
